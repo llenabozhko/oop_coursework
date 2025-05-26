d1aeee9697d5d205de75e9aed6c8d7b2ab9fb465
Olena Bozhko <bozhko.olena1123@vu.cdu.edu.ua>
2025-05-26 23:58:42 +0300

    added readme

--- obj/Debug/net8.0-windows/oop_coursework.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/oop_coursework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("oop_coursework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7da5053459386d456969b1fa050bd6d2714fd63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abaaf0661b9495ef636eb1562d64ac3323ad893")]
 [assembly: System.Reflection.AssemblyProductAttribute("oop_coursework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("oop_coursework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
